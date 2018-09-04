0a512e5d14a5f036743f0c5291406f93aa95742b
Stéphane aal <36472404+dragonbrumes@users.noreply.github.com>
2018-09-04 18:48:22 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
+var db = require('./app/config/db')
+
+const app = express()
+const port = process.env.PORT || 8080
+app.use(bodyParser.urlencoded({ useNewUrlParser: true }))
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    db = database.db("portfolio")
+    require('./app/routes')(app, db)
+})
+
+app.listen(port, () => {
+    console.log('Live on' + port)
+})
