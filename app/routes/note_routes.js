64ac699e6586591e2fa331201a76082193bca1ae
Stéphane aal <36472404+dragonbrumes@users.noreply.github.com>
2018-09-05 01:48:03 +0200

    add Create, Read One. Error on Read All

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,14 +1,61 @@
-module.exports = function (app, db) {
-    const collection =
-        app.post('/notes', (req, res) => {
-            const note = { text: req.body.body, title: req.body.title }
-            db.collection('notes').insert(note, (err, result) => {
-                if (err) {
-                    res.send({ 'error': 'An error as occured' })
-                } else {
-                    res.send(result.ops[0])
-                }
-            })
+var ObjectID = require('mongodb').ObjectID;
 
-        })
-}
+module.exports = function (app, db) {
+
+    /*************** TESTS ************/
+    // // GETS A SINGLE USER FROM THE DATABASE
+    // app.get('/notes2/:id', function (req, res) {
+    //     db.collection('notes').findById(req.params.id, function (err, user) {
+    //         if (err) return res.status(500).send("There was a problem finding the user.");
+    //         if (!user) return res.status(404).send("No user found.");
+    //         res.status(200).send(user);
+    //     });
+    // });
+
+    // // RETURNS ALL THE USERS IN THE DATABASE
+    // app.get('/notes', function (req, res) {
+    //     db.collection('notes').find({}, function (err, item) {
+    //         if (err) return res.status(500).send("There was a problem finding the users.");
+    //         res.status(200).send(item);
+    //     });
+    // });
+    /***************** fin tests */
+
+    //get One by ID
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    // get All
+    app.get('/notes', (req, res) => {
+        const id = req.params.id;
+        db.collection('notes').find({ 'title' }, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+
+    // Add one
+    app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+};
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var db = require('./app/config/db')
 
 const app = express()
 const port = process.env.PORT || 8080
-app.use(bodyParser.urlencoded({ useNewUrlParser: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err)
