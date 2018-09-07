6c06fe1d1ae13976274981eb7e1a9f77cfa442d4
Stéphane aal <36472404+dragonbrumes@users.noreply.github.com>
2018-09-07 21:20:27 +0200

    put server.js in server dir & add start script in package.json

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
-var db = require('./server/config/db')
+var db = require('./config/db')
 
 const app = express()
 const port = process.env.PORT || 8000
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
     if (err) return console.log(err)
     db = database.db("maltaweather")
-    require('./server/routes')(app, db)
+    require('./routes')(app, db)
 })
 
 app.listen(port, () => {
