ab7428e66b85b463dc2f06133ce25cf673ce26fa
Stéphane aal <36472404+dragonbrumes@users.noreply.github.com>
2018-09-10 17:06:58 +0200

    node & simple react woks together on dev

---
+++ client/src/Containers/Articles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Articles extends React.Component {
+
+
+    render() {
+        console.log('Home React')
+        return (
+            <div>Hello Articles</div>
+        )
+    }
+}
+
+export default Articles
---
+++ client/src/Containers/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+
+    render() {
+        console.log('Home React')
+        return (
+            <div>Hello React Home</div>
+        )
+    }
+}
+
+export default Home
